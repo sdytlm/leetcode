a150215198a22442e9e0e56ee1a55b8f872839b1
minli <minli@minli-Vostro-260s.(none)>
2014-10-15 14:46:32 -0400

    merge Interval

--- mergeInterval.cpp
+++ mergeInterval.cpp
@@ -9,71 +9,63 @@
 
 
 //  Definition for an interval.
-  struct Interval {
+ 
+#include<iostream>
+#include<vector>
+#include<unordered_map>
+#include<algorithm>
+using namespace std;
+
+ struct Interval {
      int start;
      int end;
      Interval() : start(0), end(0) {}
      Interval(int s, int e) : start(s), end(e) {}
   };
 
-#include<iostream>
-#include<vector>
-#include<unordered_map>
 
-using namespace std;
+bool comp(Interval a, Interval b) {
+	return a.start<b.start;
+}
 
 class Solution {
 
 public:
 
         vector<Interval> merge(vector<Interval> &intervals) {
-			  vector<Interval> result;
-              if(intervals.size()==0)
-                return result;
-              int minVal = pow(2,30);
-              int maxVal = -pow(2,31);
-              unordered_map<int, int> hash;
-              int i;
-
-              for(i=0;i<intervals.size();i++) { // store value in the hashmap
-                int startIndex = intervals[i].start;
-                int endIndex = intervals[i].end;
-                while(startIndex<=endIndex) {
-                  if(hash.find(startIndex)==hash.end())
-                    hash[startIndex] = 1;
-                  if(minVal > startIndex)
-                    minVal = startIndex;
-                  if(maxVal < startIndex)
-                    maxVal = startIndex;
-                  startIndex++;
-                }
-              }
-
-              // Read the hashmap
-        cout << minVal<<endl;
-        cout << maxVal<< endl;
-        while(minVal<=maxVal) {
-                Interval tmp;
-                while(hash.find(minVal)==hash.end()) // Find start
-                  minVal++;
-                tmp.start = minVal;
-
-                while(hash.find(minVal)!=hash.end()) // find end
-                  minVal++;
-                tmp.end = minVal-1;
-                result.push_back(tmp);
-
-              }
-              return result;
+			
+			vector<Interval> result;
+			if(intervals.size()==0 || intervals.size()==1)
+			  return intervals;
+	
+			sort(intervals.begin(), intervals.end(),comp); // sort intervals according to .start
+			result.push_back(intervals[0]);
+			vector<Interval>::iterator it;
+			for(it=intervals.begin()+1;it<intervals.end();it++) { // 若发现conflict则Update result里的.end;否则压栈
+				Interval &prev = result.back();
+				if(prev.end >= it->start) { // conflict
+					prev.end = max(prev.end, it->end);
+				}
+				else
+					result.push_back(*it);
+			}
+			
+			  return result;
 	    }
 
 };
 
+
+
 int main()
 {
     vector<Interval> a;
-    Interval i(1,3);
-    a.push_back(i);
+    Interval i(1,4);
+    Interval j(0,2);
+	Interval k(3,5);
+	a.push_back(i);
+	a.push_back(j);
+	a.push_back(k);
     Solution sol;
     sol.merge(a);
     return 0;
