edd8b93e5df59f6b5463b213bc6de11d368b723b
Min Li <lim4@vcu.edu>
2014-11-26 22:30:41 -0500

    multiplyString

--- multiplyString.cpp
+++ multiplyString.cpp
@@ -13,13 +13,15 @@ public:
   string multiply(string num1, string num2) {
     int sign1 = 1;
     int sign2 = 1;
-    if(num1[num1.size()-1]=='-') {
+    if(num1=="0"||num2=="0")
+        return "0";
+    if(num1[0]=='-') {
       sign1 = -1;
       num1.erase(0,1);
     }
-    if(num2[num2.size()-1]=='-') {
+    if(num2[0]=='-') {
       sign2 = -1;
-      num1.erase(0,1);
+      num2.erase(0,1);
     }
     int i,j,k=0;
     string result = "";
@@ -36,18 +38,27 @@ public:
         tmp += to_string(addin);
 
         // Add tmp and result
+        //cout << tmp << endl;
         result = addTwo(tmp, result,k++);
-        // cout << result << endl;
+      //  cout << result << endl;
       }
-      return result;
+      // Generate the final result
+      string final_result="";
+      if(sign1*sign2<0)
+          final_result += "-";
+      for(i=result.size()-1;i>=0;i--){
+          final_result += result[i];
+      }
+         cout << final_result.size()<<endl;
+      return final_result;
     }
 
     string addTwo(string str1, string str2, int offset){
-      cout << str1 << endl;
-      cout << str2 << endl;
-      cout << offset << endl;
+      //cout << str1 << endl;
+      //cout << str2 << endl;
+      //cout << offset << endl;
       string result="";
-      int i,j;
+      int i,j=0;
       // 把空位的挪到result里
       for(i=0;i<offset;i++)
         result += str2[i];
@@ -55,33 +66,33 @@ public:
         // 剩下的相加
         int addin = 0;
         // str2 is longer than str1
-        for(i=offset;i<str2.size();i++) {
+      for(i=offset;i<str2.size();i++) {
           int tmp;
-          if(i<str1.size())
-            tmp = (str2[i]-'0')+(str1[i]-'0')+addin;
-            else
-              tmp = (str2[i]-'0')+addin;
-              result+= to_string(tmp%10);
-              addin = tmp/10;
+          if(j<str1.size())
+            tmp = (str2[i]-'0')+(str1[j++]-'0')+addin;
+           else
+            tmp = (str2[i]-'0')+addin;
+            result+= to_string(tmp%10);
+            addin = tmp/10;
 
-            }
-            // str1 is longer than str2
-            while(i<str1.size()) {
-              int tmp;
-              tmp = (str1[i]-'0')+addin;
-              result += to_string(tmp%10);
-              addin = tmp/10;
-              i++;
-            }
-            // 把addin加上去;
-            if(addin!=0)
-              result += to_string(addin);
-              return result;
-            }
+        }
+        // str1 is longer than str2
+        while(j<str1.size()) {
+            int tmp;
+            tmp = (str1[j]-'0')+addin;
+            result += to_string(tmp%10);
+            addin = tmp/10;
+            j++;
+        }
+        // 把addin加上去;
+        if(addin!=0)
+            result += to_string(addin);
+            return result;
+        }
 
-          };
+    };
 
-          int main(){
-            Solution sol;
-            sol.multiply("123","456");
-          }
+int main(){
+    Solution sol;
+    sol.multiply("0","0");
+}
