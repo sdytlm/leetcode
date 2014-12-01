296dd230da553f0f2f5c7ad45e26f6b6a1443833
Min Li <lim4@vcu.edu>
2014-11-30 21:32:08 -0500

    subset 2

--- subset2.cpp
+++ subset2.cpp
@@ -20,6 +20,27 @@ If S = [1,2,2], a solution is:
 class Solution {
 public:
     vector<vector<int> > subsetsWithDup(vector<int> &S) {
+        vector<vector<int> > result;
+        vector<int> tmp;
+        result.push_back(tmp); // 插入空集
+        if(S.empty())
+          return result;
+        // 对S排序
+        sort(S.begin(),S.end());
+        res(result, tmp, S, 0);
+        return result;
+    }
 
+    void res(vector<vector<int> > &result, vector<int> &tmp, vector<int> &S, int index) {
+      int i;
+      for(i=index;i<S.size();i++) {
+        if(i==index || S[i]!=S[i-1]) { // 去掉重复
+          tmp.push_back(S[i]);
+          result.push_back(tmp);
+          res(result,tmp,S,i+1);
+          tmp.pop_back();
+        }
+      }
+      return ;
     }
 };
